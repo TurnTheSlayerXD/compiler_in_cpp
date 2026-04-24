4e7ec7ac8f8b5d09f3ae07a58af1de469b26a10f
Andrew <slayer2004@list.ru>
2026-04-25 02:41:42 +0300

    f

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+func panic_fmt(fmt string, args ...any) {
+	panic(fmt, args...)
+}
+
+
 type ExprId int 
 
 const (
@@ -24,67 +29,108 @@ type Token struct {
 
 
 type Context struct {
-	definedTypes []string
+	defined_types []string
 }
 
-func (self *Context) hasDefinedType(t string) bool {
-	for i:=0; i < len(self.definedTypes); i += 1 {
-		if self.definedTypes[i] == t {
+func (self *Context) has_defined_type(t string) bool {
+	for i:=0; i < len(self.defined_types); i += 1 {
+		if self.defined_types[i] == t {
 			return true
 		}
 	}
-
-
-
 	return false
 }
 
 
-type AstBuilder interface {}
-
-type Matcher interface {
-	match (ctx *Context, tokens []Token) (matches bool, astBuilder *AstBuilder)
-}
-
-type FuncMatcher struct {}
-func (self *FuncMatcher) match(ctx *Context, tokens []Token)  (matches bool, astBuilder *AstBuilder) { 
-	
-	if ok,value := ctx.getDefinedType(*tokens[0]._value) {
-		
-	}
-
-	return false, nil 
-}
-
-
-type DeclMatcher struct {}
-func (*DeclMatcher) match(ctx *Context, tokens []Token) (matches bool, astBuilder *AstBuilder) { return false, nil }
-
-
 type AstNode interface {}
-type Node struct {}
 
-func process(ctx *Context, tokens []Token, matchers []Matcher) AstNode {
-	j := 0
-	for i := 1; i < len(tokens); i += 1 {
-		for _,matcher := range matchers {
-			matches, _ := matcher.match(ctx, tokens[j:i])
+type ProducerPos struct {
+	row int
+	col int
+	index int
+}
+
+type ITokenProducer interface {
+	next_token() (ok bool, token Token)
+	get_position() ProducerPos
+	revert_position(t ProducerPos)
+	has_finished() bool 
+}
+
+type TestTokenProducer struct {
+	pos int
+	tokens []Token
+}
+
+func (self *TestTokenProducer) next_token() (ok bool, token Token) {
+	if pos < len(tokens) {
+		i += 1
+		return true, tokens[i]
+	}
+	return false, {}
+}
+func new_TextTokenProducer (tokens []Token) TestTokenProducer {
+	return TestTokenProducer{ pos: -1, tokens: tokens }
+}
+
+func (self *TestTokenProducer) get_position() ProducerPos {
+	return { index: self.pos }
+}
+func (self *TestTokenProducer) revert_position(p ProducerPos) {
+	if p.index > self.pos || p.index < -1 {
+		panic_fmt("Passed wrond self.pos=%s", p)
+	}
+	self.pos = p.index
+}
+
+func (self *TestTokenProducer) has_finished() bool {
+	return self.pos >= len(self.tokens)
+}
+
+type IAstBuilder interface {
+	consume(tokens *ITokenProducer, ctx *Context) ( matches bool, node *AstNode)
+}
+
+type struct FuncBuilder {}
+func new_FuncBuilder() *FuncBuilder {
+}
+func (self *FuncBuilder) consume(tokens *ITokenProducer, ctx *Context) ( matches bool, node *AstNode) {
+}
+
+
+
+type struct DeclAndAssignBuilder {}
+func new_DeclAndAssignBuilder() *DeclAndAssignBuilder {
+}
+func (self *DeclAndAssignBuilder) consume(tokens *ITokenProducer, ctx *Context) ( matches bool, node *AstNode) {
+
+}
+
+
+type struct OuterBuilder {}
+
+func (self *OuterBuilder) consume(tokens *ITokenProducer, ctx *Context) AstNode {
+	builders := IAstBuilder[]{ new_FuncBuilder(tokens, ctx), new_DeclAndAssignBuilder(tokens, ctx) }
+	for {
+		has_matched_any := false
+		for builder := range builders {
+			pos := tokens.get_position()
+			matches, node := builder.consume(tokens, ctx)
+			
 			if matches {
-				// node := astBuilder.buildAst()
-				j = i
+				has_matched_any = true
 				break
+			} else {
+				tokens.revert_position(pos)
 			}
 		}
+		
+		if !has_matched_any && tokens.has_finished() {
+			panic_fmt("Expected more tokens")
+		}
 	}
+}  
 
-
-	return Node{}
-}
-
-func get_tokens(programText string) []Token {
-
-	return make([]Token, 0)
-} 
 
 
 func main() {
