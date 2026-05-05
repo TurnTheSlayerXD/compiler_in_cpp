3eeb7df25e430d76f90426a641c4c937ce8f93a9
andrew <slayer2004@list.ru>
2026-05-05 20:57:03 +0300

    f

--- main.go
+++ main.go
@@ -1,144 +1,201 @@
 package main
 
-func panic_fmt(fmt string, args ...any) {
-	panic(fmt, args...)
+import (
+	"fmt"
+)
+import . "unicode"
+import "strings"
+
+
+func panic_fmt(format string, args ...any) {
+	panic(fmt.Sprintf(format, args))
 }
 
-
-type ExprId int 
-
-const (
-	Expr__Func ExprId = iota
-	Expr__Type ExprId = iota
-
-	Token__Semicolon ExprId = iota
-
-	Op__StartOneOf ExprId = iota
-	Op__EndOneOf ExprId = iota
-)
+func assert(t bool, msg string) {
+	if (t) {
+		panic(msg)
+	}
+}
 
 type EnumToken int
 const (
-	T_PLUS EnumToken = iota
+	T_SEMICOLON EnumToken = iota
+
+
+	T_TYPE_INT EnumToken 	= iota
+	T_TYPE_CHAR EnumToken 	= iota
+	T_TYPE_VOID EnumToken 	= iota
+	
+	T_OP_PLUS EnumToken 	= iota
+	T_OP_MINUS EnumToken 	= iota
+	T_OP_ASTERISK EnumToken = iota
+	T_OP_DIVIDE EnumToken 	= iota
+	T_OP_PERCENT EnumToken 	= iota
+	T_L_BR EnumToken 		= iota
+	T_R_BR EnumToken 		= iota
+	T_L_SQR EnumToken		= iota
+	T_R_SQR EnumToken		= iota
+
+	T_KWD_RETURN EnumToken  = iota
+
+
 )
 
 type Token struct {
-	_type EnumToken
-	_value *string
+	typeof EnumToken
+	
+	value string
+	
+	tag TextTag
 }
 
+type Tokenizer struct {
+	text string
+	rune_text []rune 
 
-type Context struct {
-	defined_types []string
-}
-
-func (self *Context) has_defined_type(t string) bool {
-	for i:=0; i < len(self.defined_types); i += 1 {
-		if self.defined_types[i] == t {
-			return true
-		}
-	}
-	return false
-}
-
-
-type AstNode interface {}
-
-type ProducerPos struct {
+	tokens []Token
+	
+	pos int
 	row int
 	col int
-	index int
-}
 
-type ITokenProducer interface {
-	next_token() (ok bool, token Token)
-	get_position() ProducerPos
-	revert_position(t ProducerPos)
-	has_finished() bool 
-}
+	tok_index int
+} 
 
-type TestTokenProducer struct {
+type TextTag struct {
 	pos int
-	tokens []Token
+	row int
+	col int
 }
 
-func (self *TestTokenProducer) next_token() (ok bool, token Token) {
-	if pos < len(tokens) {
-		i += 1
-		return true, tokens[i]
+
+func Tokenizer_new(text string) *Tokenizer {
+	return &Tokenizer{ text: text, tokens: [](Token){}, rune_text: []rune(text), pos: 0, row: 0, col: 0, tok_index: 0}
+}
+
+
+
+func (t *Tokenizer) trim_left() {
+	for t.pos < len([]rune(t.text)) && IsSpace(t.rune_text[t.pos]) {
+		t.incr_pos()
 	}
-	return false, {}
-}
-func new_TextTokenProducer (tokens []Token) TestTokenProducer {
-	return TestTokenProducer{ pos: -1, tokens: tokens }
 }
 
-func (self *TestTokenProducer) get_position() ProducerPos {
-	return { index: self.pos }
-}
-func (self *TestTokenProducer) revert_position(p ProducerPos) {
-	if p.index > self.pos || p.index < -1 {
-		panic_fmt("Passed wrond self.pos=%s", p)
+func (t *Tokenizer) incr_pos() {
+	assert(0 <= t.pos && t.pos < len(t.rune_text), "")
+	if t.char_at(t.pos) == '\n' {
+		t.row += 1
+		t.col = 0
+	} else {
+		t.col += 1
 	}
-	self.pos = p.index
+	t.pos += 1
 }
 
-func (self *TestTokenProducer) has_finished() bool {
-	return self.pos >= len(self.tokens)
+func (t *Tokenizer) skip_until(chars string) {
+	for t.pos < len([]rune(t.text)) && !strings.Contains(string(t.rune_text[t.pos]),chars) {
+		t.incr_pos()
+	}
 }
 
-type IAstBuilder interface {
-	consume(tokens *ITokenProducer, ctx *Context) ( matches bool, node *AstNode)
+func (t *Tokenizer) skip_until_separator() {
+	for t.pos < len([]rune(t.text)) && !IsSpace(t.rune_text[t.pos]) {
+		t.incr_pos()
+	}
 }
 
-type struct FuncBuilder {}
-func new_FuncBuilder() *FuncBuilder {
-}
-func (self *FuncBuilder) consume(tokens *ITokenProducer, ctx *Context) ( matches bool, node *AstNode) {
-}
-
-
-
-type struct DeclAndAssignBuilder {}
-func new_DeclAndAssignBuilder() *DeclAndAssignBuilder {
-}
-func (self *DeclAndAssignBuilder) consume(tokens *ITokenProducer, ctx *Context) ( matches bool, node *AstNode) {
-
+func (t *Tokenizer) substr(l int, r int) string {
+	assert(l <= r, "")
+	return string(t.rune_text[l:r])
 }
 
 
-type struct OuterBuilder {}
+func (t *Tokenizer) eof() bool {
+	return t.pos >= len(t.rune_text)
+}
 
-func (self *OuterBuilder) consume(tokens *ITokenProducer, ctx *Context) AstNode {
-	builders := IAstBuilder[]{ new_FuncBuilder(tokens, ctx), new_DeclAndAssignBuilder(tokens, ctx) }
+func (t *Tokenizer) char_at(i int) rune {
+	assert(-1 < i && i < len(t.rune_text), "")
+	return t.rune_text[i]
+}
+
+
+func (t *Tokenizer) next_tok() (ok bool, tok Token) {
+	
+	if t.tok_index < len(t.tokens) {
+		t.tok_index += 1
+		return true, t.tokens[t.tok_index - 1]
+	}
+
+
 	for {
-		has_matched_any := false
-		for builder := range builders {
-			pos := tokens.get_position()
-			matches, node := builder.consume(tokens, ctx)
-			
-			if matches {
-				has_matched_any = true
-				break
-			} else {
-				tokens.revert_position(pos)
-			}
+		t.trim_left()
+		if (t.eof()) {
+			return false, Token{}
 		}
-		
-		if !has_matched_any && tokens.has_finished() {
-			panic_fmt("Expected more tokens")
+
+		var tag_start = t.get_pos()
+
+		if t.char_at(t.pos) == '#' {
+			continue
 		}
+
+		// Проверяем единичные символы 
+		var ret Token  
+		var matched = false
+		switch t.char_at(t.pos) {
+			case '+': matched = true; ret = Token_new(T_OP_PLUS, tag_start);
+			case '-': matched = true; ret = Token_new(T_OP_MINUS, tag_start)
+			case '*': matched = true; ret = Token_new(T_OP_ASTERISK, tag_start)
+			case '/': matched = true; ret = Token_new(T_OP_DIVIDE, tag_start)
+			case '%': matched = true; ret = Token_new(T_OP_PERCENT, tag_start)
+			case '(': matched = true; ret = Token_new(T_L_BR, tag_start)
+			case ')': matched = true; ret = Token_new(T_R_BR, tag_start)
+			case '[': matched = true; ret = Token_new(T_L_SQR, tag_start)
+			case ']': matched = true; ret = Token_new(T_R_SQR, tag_start)
+			case ';': matched = true; ret = Token_new(T_SEMICOLON, tag_start)
+		}
+		if matched {
+			return true, ret
+		}
+
+		// Проверяем ключевые слова
+		t.skip_until_separator()
+		switch t.substr(tag_start.pos, t.pos) {
+			case "int": matched = true; ret = Token_new(T_TYPE_INT, tag_start)
+			case "char": matched = true; ret = Token_new(T_TYPE_CHAR, tag_start)
+			case "void": matched = true; ret = Token_new(T_TYPE_VOID, tag_start)
+			case "return": matched = true; ret = Token_new(T_KWD_RETURN, tag_start)
+		}
+		if matched {
+			return true, ret
+		}
+
+		// Проверяем строки и 
 	}
-}  
+}
+
+func Token_new(typeof EnumToken, tag TextTag) Token {
+	return Token{ typeof : typeof, tag: tag }
+}
+
+func (t *Tokenizer) get_pos() TextTag {
+	return TextTag{ pos: t.pos, row: t.row, col: t.col }
+}
+
+func (t *Tokenizer) reset_pos(text_pos TextTag) {
+
+}
 
 
 
 func main() {
 
-	programText := "int val = 1;";
+	var programText = "int val = 1;"+"";
 
-	tokens := get_tokens(programText)
 
-	ctx := Context { definedTypes: []string{}, }
-	process(&ctx, tokens, []Matcher{ })
+	tokenizer := Tokenizer_new(programText)
+
+	
+	var _ = tokenizer
 }
